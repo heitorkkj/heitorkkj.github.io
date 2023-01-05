99051c45a91a24956de2ee0a92bcf2c9bbf67495
Heitor Santos <heitorsan548@gmail.com>
2023-01-05 13:10:53 -0300

    Atualizando

---
+++ script.js
@@ -0,0 +1,22 @@
+let curtir = () =>{
+    heart = document.getElementById('heart'); 
+
+    if(heart.className === 'bi bi-heart'){
+        heart.className += '-fill'
+        heart.innerHTML = '1'
+    }else{
+        heart.className = 'bi bi-heart'
+        heart.innerHTML = ''
+    }
+}
+
+let abrirMenu = () => {
+    menu = document.getElementById('menu-principal')
+
+    if(menu.className == ''){
+        menu.className = 'active'
+    }else{
+        menu.className = ''
+    }
+    
+}
