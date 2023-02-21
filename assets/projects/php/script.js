998360971e2a7ac843407a9ddbe3091a3afe3002
Heitor Santos <heitorsan548@gmail.com>
2023-02-21 13:49:01 -0300

    updating

--- assets/projects/php/script.js
+++ assets/projects/php/script.js
@@ -2,10 +2,46 @@ let voltarPag = () =>{
     window.close()
 }
 
-let appNaoPostado = () =>{
-     alert('Desculpe-me o app não está disponivel para ser acessado')
- }
+let irPara = (local) =>{
+    let endereco = local 
 
-let appPostado = (endereco) =>{
-     window.location.href = ` http://${endereco}.rf.gd`
+    if(local == 'nenhum'){
+        alert('App não postado na web, aguarde!')
+    }else
+        window.open(endereco,'_blank')
+}
+
+
+let container = document.getElementById('projetos')
+container.innerHTML = ''
+
+let quantidadeDeProjetos = todosOsProjetosPhp.length
+
+let card = {
+    img: '',
+    titulo: '',
+    content:'', 
+    link:''
+}
+
+for(let count = 0; count<=quantidadeDeProjetos; count++){
+    card.img = todosOsProjetosPhp[count].img
+    card.titulo = todosOsProjetosPhp[count].titulo
+    card.content = todosOsProjetosPhp[count].content
+    card.link = todosOsProjetosPhp[count].link
+
+    container.innerHTML += ` 
+    <div class="card" id="card-js">
+        <div class="card-img">
+            <img src="${card.img}"/>
+        </div>
+        <h1 class="card-title"> 
+        ${card.titulo}
+        </h1>
+        <div class="card-content">
+        ${card.content}
+        </div> 
+        <button class="btn-link" onclick="irPara('${card.link}')">Visitar</button>
+    </div>`
+
 }
