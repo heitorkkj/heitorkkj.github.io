f713990915d63621604221e82a8bd5b18adadc92
Heitor Santos <heitorsan548@gmail.com>
2023-03-27 18:20:11 -0300

    feat: inserted new PHP project

--- assets/js/projetosPhp.js
+++ assets/js/projetosPhp.js
@@ -2,7 +2,7 @@ let todosOsProjetosPhp = [
     {
         img:'/assets/projects/php/img/app_help_desk.png',
         titulo:'App Help Desk',
-        content:'Aplicação para controle de gastos e orçamentos pessoais.', 
+        content:'Web aplicação para abertura e controle de chamados.', 
         link:'nenhum',
         projetoCapa:'sim'
     },
@@ -11,13 +11,20 @@ let todosOsProjetosPhp = [
         titulo:'App Send Mail',
         content:'Aplicação para enviar email de forma mais simples.', 
         link:'nenhum',
-        projetoCapa:'sim'
+        projetoCapa:'nao'
     },
     {
         img:'/assets/projects/php/img/app_lista_tarefa.png',
         titulo:'App Lista Tarefas',
         content:'App para listagem de tarefas.', 
         link:'http://app-lista-tarefas.rf.gd/',
+        projetoCapa:'sim'
+    },
+    {
+        img:'/assets/projects/php/img/app_send_file.png',
+        titulo:'App Send File',
+        content:'Web aplicação para envio de arquivos por email.', 
+        link:'http://sendfile.rf.gd/',
         projetoCapa:'sim'
     },
 ]
