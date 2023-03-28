60cfc4ff001caba0c49de1613a0ae364e390a35f
Heitor Santos <heitorsan548@gmail.com>
2023-03-27 21:18:41 -0300

    style: reworking html and css components

---
+++ assets/projects/php/app_lista_tarefas_public/assets/js/script.js
@@ -0,0 +1,63 @@
+let editar = (id, txtTarefa) => {
+    let form = document.createElement("form");
+    form.action = "tarefa_controller.php?acao=atualizar";
+    form.method = "post";
+    form.className = "row";
+
+    let inputTarefa = document.createElement("input");
+    inputTarefa.type = "text";
+    inputTarefa.name = "tarefa";
+    inputTarefa.className = "ml-2 col-8 form-control";
+    inputTarefa.value = txtTarefa;
+
+    let inputId = document.createElement("input");
+    inputId.type = "hidden";
+    inputId.name = "id";
+    inputId.value = id;
+
+    let button = document.createElement("button");
+    button.type = "submit";
+    button.className = "col-3 btn btn-info";
+    button.innerHTML = "Atualizar";
+
+    form.appendChild(inputTarefa);
+    form.appendChild(button);
+    form.appendChild(inputId);
+
+    let tarefa = document.getElementById(`tarefa_${id}`);
+    tarefa.innerHTML = "";
+
+    tarefa.insertBefore(form, tarefa[0]);
+  };
+
+  let remover = (id) => {
+    location.href = "todas_tarefas.php?acao=remover&id=" + id;
+  };
+
+  let realizada = (id) => {
+    location.href = "todas_tarefas.php?acao=realizada&id=" + id;
+};
+
+$(document).ready(() => {
+	
+    $('#new-task').on('click', () =>{
+        $('#content').load('assets/pages/new_task.php')
+    })
+
+})
+
+$(document).ready(() => {
+	
+    $('#all-tasks').on('click', () =>{
+        $('#content').load('assets/pages/all_tasks.php')
+    })
+
+})
+
+$(document).ready(() => {
+	
+    $('#pendent-tasks').on('click', () =>{
+        $('#content').load('assets/pages/pendent_tasks.php')
+    })
+
+})
