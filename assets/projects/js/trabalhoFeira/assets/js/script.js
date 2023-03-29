2d627efb35a965a9eb0e20ff34cca68398bef02a
Heitor Santos <heitorsan548@gmail.com>
2023-03-29 20:10:02 -0300

    style:  changing the code pattern and applying clean code principles in the Pandemic Simulator

--- assets/projects/js/trabalhoFeira/assets/js/script.js
+++ assets/projects/js/trabalhoFeira/assets/js/script.js
@@ -1,20 +1,21 @@
 let resizeWindow = () => {
-  let largura = window.innerWidth;
-  let altura = window.innerHeight;
-  let container = document.querySelector(".container");
+  let width = window.innerWidth;
+  let height = window.innerHeight;
+  let main = document.querySelector("#main");
 
-  if (largura < 780) {
-    container.innerHTML = "";
-    container.innerHTML = `<div class="responsiv"><img src="./imagens/fixing.svg"/>Simulador não disponivel para mobile!</div>`;
+  if (width < 780) {
+    main.innerHTML = "";
+    main.innerHTML = `<div class="responsiv"><img src="./assets/images/fixing.svg"/>Simulador não disponivel para mobile!</div>`;
   }
 };
 
-let taxa = 3.18;
-taxa = document.getElementById("taxa").innerHTML = taxa.toFixed(2);
+let rate = 3.18;
+let spanRate = document.getElementById("taxa")
+spanRate.innerHTML = rate.toFixed(2);
 
-let popSaudavel = (total, taxa) =>{
+let popSaudavel = (total, rate) =>{
 
-    var contam = 1000000*taxa; 
+    var contam = 1000000*rate; 
     total = total - contam;
     if(total <= 0){
         total = 0
@@ -25,20 +26,21 @@ let popSaudavel = (total, taxa) =>{
     
 }
 
-let enableMascara = () =>{
-    if(taxa>=3){
-    taxa -= 1
-    }
-}
+let enableAction = (type) =>{
 
-let enableDistanciamento = () =>{
-    if(taxa>=2){
-        taxa -= 0.7
+    if(type == 'mascara'){
+        if(rate>=3){
+            rate -= 1
+        }
+    }else if(type == 'distanciamento'){
+        if(rate>=2){
+            rate -= 0.7
+        }
+    }else{
+        if(rate>=1.19){
+            rate -= 0.3
+        }
     }
-}
 
-let enableHigene = () =>{
-    if(taxa>=1){
-        taxa -= 0.3
-    }
+    spanRate.innerHTML = rate.toFixed(2)
 }
