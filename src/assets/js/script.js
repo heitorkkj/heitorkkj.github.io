22bd0dc52c1a29b947e49d27dde60fe69ca6e5b4
Heitor Santos <heitorsan548@gmail.com>
2023-06-07 14:41:26 -0300

    fix (folder structure): new project folder structure (clear and clean structure)

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1,24 +1,5 @@
-
-$(document).ready(() => {
-    $('#btn-sobre').on('click', () =>{
-        $('#sobre').load('sobre.md')
-    })
-})
-
 let abrirMenu = () => {
     menu = document.getElementById('navbar-menu')
 
     menu.className = menu.className == '' ? 'active' : ''    
 }
-
-let email = () =>{
-    areaEmail = document.getElementById('area-email')
-    areaTexto = document.getElementById('email-texto')
-
-    if(areaTexto.innerText == ''){
-        areaTexto.innerText = 'heitorsan548@gmail.com'
-    }else{
-        areaTexto.innerText = ''
-    }
-}
-
